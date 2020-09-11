5c11031065caf973dc762b943fac098d30d314fc
vasiliypapchanski <70929136+vasiliypapchanski@users.noreply.github.com>
2020-09-10 20:19:58 -0400

    Add files via upload

---
+++ omni_S5_Pro.mk
@@ -0,0 +1,39 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Add this line if your device is 64-bit
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# Otherwise, If you have 32-bit device, add the below line instead of above line
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+
+# Another common config inclusion
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+
+# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Replace $$DEVICE$$ with your Device Name's Value.
+# Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
+PRODUCT_COPY_FILES += device/$$BRAND$$/$$DEVICE$$/prebuilt/zImage:kernel
+# Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
+# PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/*:root/*
+
+PRODUCT_BRAND := UMIDIGI
+PRODUCT_DEVICE := S5_Pro
+PRODUCT_MANUFACTURER := UMIDIGI
+PRODUCT_MODEL := S5_Pro
+PRODUCT_NAME := omni_S5_Pro
+
+# Forcefully add mtp support (adb is already there)
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# Add fingerprint from Stock ROM build.prop
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.treble.enabled=true \
+    persist.sys.usb.config=mtp \
+    persist.service.adb.enable=1 \
+    persist.service.debuggable=1 \
+    ro.secure=1 \
+    ro.adb.secure=0 \
+    ro.debuggable=1 \
+    ro.allow.mock.location=0
